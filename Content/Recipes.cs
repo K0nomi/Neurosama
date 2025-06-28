cc6442bd751ddc0560dbe93cb23fdfe84bc30652
Konomi <konomik14@gmail.com>
2025-06-28 20:16:27 +1000

    Add gold ermfish -> golden delight recipe

---
+++ Content/Recipes.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Neurosama.Content
+{
+    public class Recipes : ModSystem
+    {
+        public override void AddRecipes()
+        {
+            // Gold ermfish to golden delight
+            Recipe.Create(ItemID.GoldenDelight)
+                .AddIngredient<Items.Consumables.ErmFishGold>()
+                .AddTile(TileID.CookingPots)
+                .Register();
+        }
+    }
+}
